3241c8ca683823af77e3b45ab119ebe7d480bc5f
Vasyaproger <vasyaproger97@gmail.com>
2025-07-25 14:41:24 +0600

    78787444

--- src/config/database.js
+++ src/config/database.js
@@ -4,10 +4,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: vh438.timeweb.ru,
+  user: ch79145_social,
+  password: Vasya11091109,
+  database: ch79145_social,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
