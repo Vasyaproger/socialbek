d0d027bcfb171653f9ac286bb772da66f90b3a31
Vasyaproger <vasyaproger97@gmail.com>
2025-07-25 18:36:54 +0600

    9999991111

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,25 @@
 const mysql = require('mysql2/promise');
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 const pool = mysql.createPool({
-  host: vh438.timeweb.ru,
-  user: ch79145_social,
-  password: Vasya11091109,
-  database: ch79145_social,
+  host: 'vh438.timeweb.ru', // Добавлены кавычки
+  user: 'ch79145_social',   // Добавлены кавычки
+  password: 'Vasya11091109', // Добавлены кавычки
+  database: 'ch79145_social', // Добавлены кавычки
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
+
+// Проверяем подключение к базе данных
+(async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log('Успешное подключение к базе данных MySQL');
+    connection.release();
+  } catch (error) {
+    console.error('Ошибка подключения к базе данных:', error.message);
+    process.exit(1); // Завершаем процесс при ошибке
+  }
+})();
 
 module.exports = pool;
